4f1bd232b0723aa9939eaecb26d061912e49111c
Melon Lo <dp98062@gmail.com>
2023-08-18 01:06:10 +0800

    Optimize SideBar and Tweets component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,19 +1,50 @@
 import './SideBar.scss'
+import LogoIcon from 'assets/icons/logo@2x.png'
+import HomeIcon from 'assets/icons/home_2@2x.png'
+import UserIcon from 'assets/icons/user_2@2x.png'
+import CogIcon from 'assets/icons/cog_2@2x.png'
+import LogoutIcon from 'assets/icons/logout@2x.png'
 
 export const SideBar = () => {
   return (
     <div className="sideBarContainer">
       <div className="topSection">
-        <div className="logoBox">Logo</div>
-          <div className="functions">
-            <div className="function">首頁</div>
-            <div className="function">個人資料</div>
-            <div className="function">設定</div>
-          </div>
+        <div className="logoBox"><img className="logo" src={LogoIcon} alt="logoIcon" /></div>
+        <div className="pages">
+            <div className="page">
+              <div className="iconBox">
+                <img className="icon" src={HomeIcon} alt="homeIcon" />
+              </div>
+              <div className="pageTitle">
+                首頁
+              </div>
+            </div>
+            <div className="page">
+              <div className="iconBox">
+                <img className="icon" src={UserIcon} alt="userIcon" />
+              </div>
+              <div className="pageTitle">
+                個人資料
+              </div>
+            </div>
+            <div className="page">
+              <div className="iconBox">
+                <img className="icon" src={CogIcon} alt="cogIcon" />
+              </div>
+              <div className="pageTitle">
+                設定
+              </div>
+            </div>
+        </div>
         <button className="tweetsButton">推文</button>
       </div>
       <div className="bottomSection">
-        <div className="logoutBox">登出</div>
+        <div className="iconBox">
+          <img className="icon" src={LogoutIcon} alt="logoutIcon" />
+        </div>
+        <div className="title">
+          登出
+        </div>
       </div>
     </div>
 

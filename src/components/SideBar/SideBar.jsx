7cbc603a5c9cffeeaaefd3a25d037435a01d80f7
Melon Lo <dp98062@gmail.com>
2023-08-21 02:25:08 +0800

    Add UserPage logic

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,24 @@
 import './SideBar.scss'
 
+// import dependencies
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+
 // import icons
 import { ReactComponent as LogoIcon } from 'assets/icons/logo.svg'
 import { ReactComponent as HomeHollowIcon } from 'assets/icons/home_hollow.svg'
+import { ReactComponent as HomeIcon } from 'assets/icons/home.svg'
 import { ReactComponent as UserHollowIcon } from 'assets/icons/user_hollow.svg'
+import { ReactComponent as UserIcon } from 'assets/icons/user.svg'
 import { ReactComponent as CogHollowIcon } from 'assets/icons/cog_hollow.svg'
+import { ReactComponent as CogIcon } from 'assets/icons/cog.svg'
 import { ReactComponent as LogoutIcon } from 'assets/icons/logout.svg'
+import { PageContext } from 'context/PageContext'
 
 export const SideBar = () => {
+  const navigate = useNavigate()
+  const { page, setPage } = useContext(PageContext)
+
   return (
     <div className="sideBarContainer">
       <div className="topSection">
@@ -15,30 +26,39 @@ export const SideBar = () => {
           <LogoIcon />
         </div>
         <div className="pages">
-            <div className="page">
-              <div className="iconBox">
-                <HomeHollowIcon />
-              </div>
-              <div className="pageTitle">
-                首頁
-              </div>
+          <div className="page" onClick={() => {
+            navigate('/home')
+            setPage('home')
+          }}>
+            <div className="iconBox">
+              {page === 'home' ? <HomeIcon className="activeIcon" /> : <HomeHollowIcon />}
             </div>
-            <div className="page">
-              <div className="iconBox">
-                <UserHollowIcon />
-              </div>
-              <div className="pageTitle">
-                個人資料
-              </div>
+            <div className={page === 'home' ? 'activePageTitle' : 'pageTitle'}>
+              首頁
             </div>
-            <div className="page">
-              <div className="iconBox">
-                <CogHollowIcon />
-              </div>
-              <div className="pageTitle">
-                設定
-              </div>
+          </div>
+          <div className="page" onClick={() => {
+            navigate('/user/self')
+            setPage('user')
+          }}>
+            <div className="iconBox">
+              {page === 'user' ? <UserIcon className="activeIcon" /> : <UserHollowIcon />}
             </div>
+            <div className={page === 'user' ? 'activePageTitle' : 'pageTitle'}>
+              個人資料
+            </div>
+          </div>
+          <div className="page" onClick={() => {
+            navigate('/setting')
+            setPage('setting')
+          }}>
+            <div className="iconBox">
+              {page === 'setting' ? <CogIcon className="activeIcon" /> : <CogHollowIcon />}
+            </div>
+            <div className={page === 'setting' ? 'activePageTitle' : 'pageTitle'}>
+              設定
+            </div>
+          </div>
         </div>
         <button className="tweetsButton">推文</button>
       </div>

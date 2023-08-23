14f2c5028e6b0fe6bcd6c9bf80eda3ee3eff39bd
Melon Lo <dp98062@gmail.com>
2023-08-24 01:34:43 +0800

    Add setting logic

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,6 +1,78 @@
 import './Setting.scss'
+import { useState } from 'react'
+
+// 信箱格式規範
+const emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
 
 export const Setting = () => {
+  // 設置每個 input 的狀態
+  const [account, setAccount] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordCheck, setPasswordCheck] = useState('')
+
+  // 設置驗證狀態
+  const [accountIsValid, setAccountIsValid] = useState(false)
+  const [nameIsValid, setNameIsValid] = useState(false)
+  const [emailIsValid, setEmailIsValid] = useState(false)
+
+  // 設置驗證提示，若沒通過則跳出
+  const [accountAlert, setAccountAlert] = useState(false)
+  const [nameAlert, setNameAlert] = useState(false)
+  const [emailAlert, setEmailAlert] = useState(false)
+
+  // 驗證函式，每次input改變時執行
+  function checkAccount(inputValue) {
+    // 如果帳號的第一個字符不是@，不通過
+    if(inputValue.indexOf('@') !== 0) {
+      return setAccountAlert(true)
+    } else if(inputValue.trim().length > 20) {
+      return setAccountAlert(true)
+    } else {
+      setAccountAlert(false)
+      setAccountIsValid(true)
+    }
+  }
+  function checkName(inputValue) {
+    // 如果名字大於50個字符，不通過
+    if(inputValue.trim().length > 50) {
+      setNameAlert(true)
+    } else {
+      setNameAlert(false)
+      setNameIsValid(true)
+    }
+  }
+  function checkEmail(inputValue) {
+    // 如果信箱不符合emailRule，不通過
+    if(!emailRule.test(inputValue)) {
+      setEmailAlert(true)
+    } else {
+      setEmailAlert(false)
+      setEmailIsValid(true)
+    }
+  }
+
+  // 表單送出函式，當驗證全通過時才會送出
+  function onFormSubmit(e) {
+    e.preventDefault()
+
+    // 若「密碼」與「密碼再確認」不相同，不送出
+    if(password !== passwordCheck) {
+      return alert("兩次輸入的密碼不相符！")
+    }
+
+    // console.log 出資料
+    console.log(account, name, email, password)
+
+    // 送出後清空
+    setAccount('')
+    setName('')
+    setEmail('')
+    setPassword('')
+    setPasswordCheck('')
+  }
+
   return (
     <div className="settingContainer">
       <div className="topSection">
@@ -10,27 +82,75 @@ export const Setting = () => {
         <form className="settingForm">
           <div className="input">
             <div className="inputLabel">帳號</div>
-            <input name="account" className="inputBox" type="text" />
+            <input 
+              name="account" 
+              className="inputBox" 
+              type="text" 
+              onChange={(e) => {
+                checkAccount(e.target.value)
+                setAccount(e.target.value)
+              }}
+              value={account}
+            />
+            {accountAlert && <div className='validationAlert'>帳號不包含前綴「@」或是字數超過上限！（最多20字）</div>}
           </div>
           <div className="input">
             <div className="inputLabel">名稱</div>
-            <input name="name" className="inputBox" type="text" />
+            <input 
+              name="name" 
+              className="inputBox" 
+              type="text" 
+              onChange={(e) => {
+                checkName(e.target.value)
+                setName(e.target.value)
+              }}
+              value={name}
+            />
+            {nameAlert && <div className='validationAlert'>字數超過上限！（最多50個字）</div>}
           </div>
           <div className="input">
             <div className="inputLabel">Email</div>
-            <input name="email" className="inputBox" type="text" />
+            <input 
+              name="email" 
+              className="inputBox" 
+              type="text" 
+              onChange={(e) =>{
+                checkEmail(e.target.value)
+                setEmail(e.target.value)
+              }}
+              value={email}
+            />
+            {emailAlert && <div className='validationAlert'>信箱不符合規定格式！</div>}
           </div>
           <div className="input">
             <div className="inputLabel">密碼</div>
-            <input name="password" className="inputBox" type="password" />
+            <input 
+              name="password" 
+              className="inputBox" 
+              type="password" 
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </div>
           <div className="input">
             <div className="inputLabel">密碼再確認</div>
-            <input name="passwordCheck" className="inputBox" type="password" />
+            <input 
+              name="passwordCheck" 
+              className="inputBox" 
+              type="password" 
+              onChange={(e) => setPasswordCheck(e.target.value)}
+              value={passwordCheck}
+            />
           </div>
         </form>
         <div className="buttonBox">
-          <button className="save">儲存</button>
+          {/* 若驗證未通過，送出鈕樣式不同，點按也不會送出 */}
+          <button 
+            className={accountIsValid & nameIsValid & emailIsValid ? 'valid' : 'invalid'}
+            onClick={(e) => onFormSubmit(e)}
+          >
+            儲存
+          </button>
         </div>
       </div>
     </div>

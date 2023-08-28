31ad2fbac297e0b205aa028700ab34eb9ee14f7b
susu725 <zxcv1010017@gmail.com>
2023-08-28 17:26:39 +0800

    feat: add followship api to home page recommendation

--- src/components/RecommendationList/RecommendationList.jsx
+++ src/components/RecommendationList/RecommendationList.jsx
@@ -2,12 +2,19 @@ import './RecommendationList.scss'
 
 import { Recommendation } from './Recommendation'
 
+const fake = [{id:1},{id:2},{id:3},{id:4},{id:5}]
+
 export const RecommendationList = () => {
+
+  const recommendations = fake.map(recommendation=>{
+    return <Recommendation key={recommendation.id} recommendation={recommendation}/>
+  })
+
   return (
     <div className="recommendationContainer">
       <div className="title">推薦跟隨</div>
       <div className="people">
-        <Recommendation />
+        {recommendations}
       </div>
     </div>
   )

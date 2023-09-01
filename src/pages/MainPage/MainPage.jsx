c27dc4cb530ba86e1f4de7d0e0e54db5ac31392d
andrewye0128 <b0936486128@gmail.com>
2023-09-01 18:11:23 +0800

    set change page function

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,17 +4,28 @@ import { TweetList } from "components/TweetList/TweetList";
 import { RecommendationList } from 'components/RecommendationList/RecommendationList';
 import { useContext, useEffect, useState } from 'react';
 import { ModalContext } from 'context/ModalContext';
+import { useNavigate } from 'react-router-dom';
 
 // API
 import { getAllTweets } from 'api/tweets';
+import { AuthContext } from 'context/AuthContext';
 
 export const MainPage = () => {
   // 存放tweets
   const [tweets, setTweets] = useState([])
-
   const { showModal, showReplyModal } = useContext(ModalContext)
+  const { isAuthenticated } = useContext(AuthContext)
+  const navigate = useNavigate()
+  
+  let userInfo = {}
+  let role = ""
+  if (localStorage.getItem("userInfo")) {
+    userInfo = JSON.parse(localStorage.getItem("userInfo"))
+    role = userInfo.role
 
-  useEffect(() => async () => {
+  } 
+
+  useEffect = (() => async () => {
     const getTweetsAsync = async () => {
       try {
         const tweets = await getAllTweets()
@@ -24,8 +35,15 @@ export const MainPage = () => {
       }
     }
 
-    getTweetsAsync()
-  })
+      if( role === "user") {
+        getTweetsAsync()
+      } else if (role === "admin") {
+        navigate("/admin_main")
+      } else {
+        navigate("/login")
+      }
+
+  }, [])
 
   return (
     <div className="mainPageContainer">

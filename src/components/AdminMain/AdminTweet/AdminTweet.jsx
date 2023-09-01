345613419fa10b378d0d09d8e6d9d96bd9a08d1d
andrewye0128 <b0936486128@gmail.com>
2023-09-01 22:45:18 +0800

    intall admintweet again

--- src/components/AdminMain/AdminTweet/AdminTweet.jsx
+++ src/components/AdminMain/AdminTweet/AdminTweet.jsx
@@ -1,4 +1,6 @@
 import './AdminTweet.scss'
+import { useState, useEffect } from 'react'
+import { getAllTweetsAdmin, deleteTweetAdmin } from 'api/tweets'
 import { ReactComponent as CloseIcon } from 'assets/icons/close.svg'
 
 export const TweetTitle = ({ title }) => {
@@ -6,25 +8,39 @@ export const TweetTitle = ({ title }) => {
 }
 
 // TweetItem 子元件
-export const AdminTweetItem = () => {
+export const AdminTweetItem = ({
+  avatar,
+  tweet,
+  name,
+  account,
+  time,
+  content,
+  onClick,
+}) => {
+
+  const authToken = localStorage.getItem("authToken")
+
+
   return (
     <div className="AdminTweetItem">
       <div className="avatarBox">
-        <img className="avatar" src="https://www.pokemongoplusplus.com/img/img-2.jpg" alt="avatar" />
+        <img className="avatar" src={avatar} alt="avatar" />
       </div>
       <div className="tweetText">
         <div className="topText">
           <div className="infoText">
-            <div className="name">Chris</div>
-            <div className="account">@Anna</div>
-            <div className="time">·3小時</div>
+            <div className="name">{name}</div>
+            <span className="smallText">@{account}·{time}</span>
           </div>
           <div className='closeBox'>
-            <CloseIcon className='close' />
+            <CloseIcon
+              className='close'
+              onClick={() => onClick?.({ authToken: authToken, id: tweet.id })}
+            />
           </div>
         </div>
         <div className="tweetContent">
-          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
+          {content}
         </div>
       </div>
     </div>
@@ -33,21 +49,55 @@ export const AdminTweetItem = () => {
 
 // TweetList 父元件
 export const AdminTweetList = () => {
+  const [tweets, setTweets] = useState([])
+
+  // 刪除特定推文函式
+  const hacndleDelete = async ({ authToken, id }) => {
+    try {
+      const res = await deleteTweetAdmin(authToken, id)
+      setTweets((prevTweets) => prevTweets.filter((tweet) => tweet.id !== id))
+
+      console.log("deleteTweetAdmin", res)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+
+  useEffect(() => {
+
+    // 透過 API 撈初始資料：管理員可見的全部推文
+    const getAllTweetsAdminAsync = async () => {
+      try {
+        const datas = await getAllTweetsAdmin()
+        setTweets(datas.map((data) => ({ ...data })))
+        // console.log('setTweets後的值', datas)
+
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getAllTweetsAdminAsync()
+  }, [])
+
   return (
     <div className='TweetListWrapper'>
-      <TweetTitle title="推特清單" />
+      <TweetTitle title="推文清單" />
       <div className='TweetListContent'>
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
-        <AdminTweetItem />
+        {tweets.map((tweet) => (
+          <AdminTweetItem
+            key={tweet.id}
+            avatar={tweet.userAvatar}
+            tweet={tweet}
+            name={tweet.userName}
+            account={tweet.userAccount}
+            time={tweet.createdAt}
+            content={tweet.description}
+            onClick={({ authToken, id }) => hacndleDelete({ authToken, id })}
+          />
+        ))}
       </div>
     </div>
   )
